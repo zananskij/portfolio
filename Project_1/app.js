e1032b93ca07091d93446878e0a7a961e56d2098
Jon <zananskij@gmail.com>
2022-11-07 00:38:46 -0500

    v1 final updated

--- Project_1/app.js
+++ Project_1/app.js
@@ -7,16 +7,3 @@ $(() => {
     }
   })
 })
-
-var listTitle = document.getElementsByClassName('list-title')
-var listContent = document.getElementsByClassName('list-content')
-function openList(listname) {
-  for (tablink of listTitle) {
-    tablink.classList.remove('active-btn')
-  }
-  for (tabcontent of listContent) {
-    tabcontent.classList.remove('active-list')
-  }
-  event.currentTarget.classList.add('active-btn')
-  document.getElementById(listname).classList.add('active-list')
-}

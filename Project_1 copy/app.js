6c504889850e7f25064f4bdbadaa3d5cf782b34a
zananskij <zananskij@gmail.com>
2022-11-08 09:31:07 -0500

    moved all files to root

--- Project_1 copy/app.js
+++
@@ -1,9 +0,0 @@
-$(() => {
-  $(window).on('scroll', function () {
-    if ($(window).scrollTop()) {
-      $('nav').addClass('black')
-    } else {
-      $('nav').removeClass('black')
-    }
-  })
-})

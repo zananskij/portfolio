e28e24848e23ba0e6bd4e5fb7bc2c7f62c06270e
zananskij <zananskij@gmail.com>
2022-11-23 00:00:02 -0500

    added changes + css

---
+++ crud_app/app.js
@@ -0,0 +1,11 @@
+// PULL DATA FROM AN API
+
+// const searchBox = $('.')
+
+// $(() => {
+//     $('p#skill').click(function (event) {
+//       $(this).addClass('active-btn').siblings().removeClass('active-btn')
+//       $('div#skills').addClass('active-list')
+//       $('div#experience').removeClass('active-list')
+//       $('div#education').removeClass('active-list')
+//     })
